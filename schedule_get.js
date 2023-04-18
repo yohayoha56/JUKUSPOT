c18b0964a463c91033057d2f11655ae6d32274bd
ryohey <ryohey0405@gmail.com>
2023-04-18 17:08:25 +0900

    a

--- schedule_get.js
+++ schedule_get.js
@@ -19,10 +19,52 @@ function schedule_get(e) {
   .then(data => {
     var target = document.getElementById("page-content");
     target.innerHTML =data;
+    modifyTable()
     })
 };
+
 // li要素を取得し、クリックイベントを登録
 var liElements = document.querySelectorAll('#shift-menu .child-menu li');
 liElements.forEach(li => {
   li.addEventListener('click', schedule_get);
 });
+
+
+function modifyTable() {
+  const tableRows = document.querySelectorAll('table tbody tr:not(:first-child)');
+
+  tableRows.forEach(row => {
+    const availabilityCell = row.querySelector('td:nth-child(2)');
+    const requestStatusCell = row.querySelector('td:nth-child(5)');
+
+    // 勤務可否列の変更
+    switch (availabilityCell.textContent) {
+      case '勤務不可':
+        availabilityCell.innerHTML = '<button class="unavailable">勤務不可</button>';
+        break;
+      case '調整中':
+        availabilityCell.innerHTML = '<button class="adjusting">調整中</button>';
+        break;
+      case '講師回答前':
+        availabilityCell.innerHTML = '<button class="adjusting">講師回答前</button>';
+        break;
+      case '一部勤務可能':
+        availabilityCell.innerHTML = '<button class="partially-available">一部勤務可能</button>';
+        break;
+      case '終日勤務可能':
+        availabilityCell.innerHTML = '<button class="available">終日勤務可能</button>';
+        break;
+    }
+
+    // 依頼状況列の変更
+    if (availabilityCell.textContent === '勤務不可') {
+      requestStatusCell.innerHTML = '<button class="request-not-possible">依頼不可</button>';
+    } else if (requestStatusCell.textContent === '勤務確定') {
+      requestStatusCell.innerHTML = '<button class="confirmed">勤務確定</button>';
+    } else if (requestStatusCell.textContent === '講師回答前' || requestStatusCell.textContent === '調整希望') {
+      requestStatusCell.innerHTML = '<button class="modify">依頼内容を変更</button>';
+    } else {
+      requestStatusCell.innerHTML = '<button class="request">勤務を依頼する</button>';
+    }
+  });
+}
