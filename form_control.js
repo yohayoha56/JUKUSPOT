dcf3bebec59382a7dfdce0512ad2fff86f08f676
ryohey <ryohey0405@gmail.com>
2023-04-18 21:52:41 +0900

    a

--- form_control.js
+++ form_control.js
@@ -65,9 +65,18 @@ const handleSubmit = async (event) => {
         data[key] = value;
     }
     // 勤務開始時間、終了時間、休憩時間をdataに追加
-    data["勤務開始時間"] = data["start_hour"] + ':' + data["start_minute"];
-    data["勤務終了時間"] = data["end_hour"] + ':' + data["end_minute"];
+    data["勤務開始時間"] = data["start_hour"] + ':' + (data["start_minute"] === '0' ? '00' : data["start_minute"]);
+    data["勤務終了時間"] = data["end_hour"] + ':' + (data["end_minute"] === '0' ? '00' : data["end_minute"]);
     data["休憩時間"] = data["break_time"];
+
+    // 日本時間のタイムスタンプを追加
+    data["submitted_timestamp"] = new Date().toLocaleString("ja-JP");
+
+    // 勤務日を取得
+    data["work_date"] = workDate;
+
+    // 依頼取り消しを追加
+    data["request_cancellation"] = false;
     // 補足・備考のテキストに追記
     data["remarks"] = "[教室から｜シフト依頼時]\n" + data["remarks"];
     console.log(data);
@@ -93,12 +102,35 @@ form.addEventListener('submit', handleSubmit);
 
 
 
-  // 依頼取り消しの操作
-  function handleRequestCancellation() {
+// 依頼取り消しの操作
+function handleRequestCancellation() {
     if (confirm("本当に依頼を取り消しますか？")) {
-      const data = { "依頼取り消し": true };
+      const data = {
+        "依頼取り消し": true,
+        "work_date": workDate
+      };
       console.log(data);
-      // ここでフォームのデータとして送信
+  
+      // POSTリクエストを送信
+      (async () => {
+        const response = await fetch("", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+          body: JSON.stringify(data),
+          mode: 'cors',
+        });
+  
+        const text = await response.text();
+        if (text.includes("Success")) {
+          console.log('データが正常に送信されました');
+          form.reset(); // フォームの入力値をリセット
+        } else {
+          console.error(`エラーメッセージ: ${text}`);
+        }
+      })();
+  
       closeModal();
     }
   }
