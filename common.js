d30214c83a052b838f4645fc5afa11f6951cf0ae
ryohey <ryohey0405@gmail.com>
2023-04-19 16:39:47 +0900

    a

--- common.js
+++ common.js
@@ -1,27 +1,35 @@
 // すべてのプロパティ要素を取得
 const properties = document.querySelectorAll('.notion-page__property');
+console.log('プロパティ要素:', properties);
 
 // 新しいデータを格納する空のオブジェクトを作成
 const newData = {};
 
 // 各プロパティ要素に対して処理を実行
-properties.forEach(property => {
-  const keyElement = property.querySelector('.notion-property__key');
-  const valueElement = property.querySelector('.notion-property__value');
-  const rollupElement = property.querySelector('.notion-property__rollup');
+properties.forEach((property, index) => {
+  console.log(`プロパティ要素 ${index}:`, property);
+
+  const keyElement = property.querySelector('.notion-page__property-name');
+  const valueElement = property.querySelector('.notion-property__text');
+  const rollupElement = property.querySelector('.notion-property__relation');
 
   if (keyElement) {
     const key = keyElement.textContent.trim();
+    console.log(`キー要素 ${index}:`, keyElement, key);
 
     if (valueElement) {
-      newData[key] = valueElement.textContent.trim();
+      const value = valueElement.textContent.trim();
+      newData[key] = value;
+      console.log(`値要素 ${index}:`, valueElement, value);
     } else if (rollupElement) {
       const rollupData = [];
       const links = rollupElement.querySelectorAll('.notion-link');
+      console.log(`ロールアップ要素 ${index}:`, rollupElement, links);
 
       links.forEach(link => {
         const title = link.textContent.trim();
         rollupData.push(title);
+        console.log('リンク要素:', link, title);
       });
 
       newData[key] = rollupData;
@@ -32,4 +40,4 @@ properties.forEach(property => {
 });
 
 // 整形された newData オブジェクトをコンソールに出力
-console.log(newData);
+console.log('新しいデータ:', newData);
