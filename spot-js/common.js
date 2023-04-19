1cf8b4f4276278b855a8ebf32678d8e222c70a4b
ryohey <ryohey0405@gmail.com>
2023-04-20 00:21:46 +0900

    a

--- spot-js/common.js
+++ spot-js/common.js
@@ -40,23 +40,20 @@ const newData = extractProperties();
 
 
 // サイドメニューのコントロールスクリプト
-document.addEventListener("DOMContentLoaded", function() {
-  const menuItems = document.querySelectorAll(".group-menu");
+const menuItems = document.querySelectorAll(".group-menu");
 
-  menuItems.forEach((menuItem) => {
-    menuItem.querySelector(".menu-item").addEventListener("click", () => {
-      const childMenu = menuItem.querySelector(".child-menu");
+menuItems.forEach((menuItem) => {
+  menuItem.querySelector(".menu-item").addEventListener("click", () => {
+    const childMenu = menuItem.querySelector(".child-menu");
 
-      // 他の子メニューを閉じる
-      menuItems.forEach((otherMenuItem) => {
-        if (otherMenuItem !== menuItem) {
-          otherMenuItem.querySelector(".child-menu").style.display = "none";
-        }
-      });
-
-      // クリックされた子メニューの表示状態を切り替える
-      childMenu.style.display = childMenu.style.display === "block" ? "none" : "block";
+    // 他の子メニューを閉じる
+    menuItems.forEach((otherMenuItem) => {
+      if (otherMenuItem !== menuItem) {
+        otherMenuItem.querySelector(".child-menu").style.display = "none";
+      }
     });
+
+    // クリックされた子メニューの表示状態を切り替える
+    childMenu.style.display = childMenu.style.display === "block" ? "none" : "block";
   });
 });
-
