ae004743efd563a33b3cf63c914ba39348c0e1a6
ryohey <ryohey0405@gmail.com>
2023-04-20 23:51:07 +0900

    a

--- spot-js/load.js
+++ spot-js/load.js
@@ -42,8 +42,8 @@ waitForProperties().then((propertiesContainer) => {
 
     // 'https://heys45.github.io/SAUCEED/spot-js/form_control.js',
     const schoolScripts = [
-      'https://heys45.github.io/SAUCEED/spot-js/school_page_call.js',
       'https://heys45.github.io/SAUCEED/spot-js/school_page_make.js',
+      'https://heys45.github.io/SAUCEED/spot-js/school_page_call.js',
       'https://heys45.github.io/SAUCEED/spot-js/schedule_get.js',
     ];
     schoolScripts.forEach(loadScript);
