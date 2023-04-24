d39816df12cf2e2f4b4f74ea316b08d0b118de6f
ryohey <ryohey0405@gmail.com>
2023-04-24 10:19:03 +0900

    a

--- spot-js/load.js
+++ spot-js/load.js
@@ -24,7 +24,6 @@ waitForProperties().then((propertiesContainer) => {
   const scripts = [
     'https://heys45.github.io/SAUCEED/spot-js/page_make.js',
     'https://heys45.github.io/SAUCEED/spot-js/common.js',
-    'https://heys45.github.io/SAUCEED/spot-js/page_call.js',
     ...(newData["ページタイプ"] === "school" ? [
       'https://heys45.github.io/SAUCEED/spot-js/schedule_get.js',
     ] : []),
