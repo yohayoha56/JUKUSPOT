b360d0200f507c55b9b263138cc44daee5d96282
ryohey <ryohey0405@gmail.com>
2023-05-07 15:02:38 +0900

    a

--- spot-js/page_top.js
+++ spot-js/page_top.js
@@ -156,7 +156,7 @@ rows.forEach(row => {
 
 
 // フォームなしの時、見出し削除ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-const h2Element = document.querySelector(".forms-container h2");
+const h2Element = document.querySelector("#forms-container h2");
 
 if (!document.querySelector("#forms-container .form-container")) {
     h2Element.style.display = "none";
