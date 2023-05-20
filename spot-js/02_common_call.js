3b4c7e791c399463fcfa82f167df469f4974eba0
ryohey <ryohey0405@gmail.com>
2023-05-21 07:01:10 +0900

    a

--- spot-js/02_common_call.js
+++ spot-js/02_common_call.js
@@ -1,20 +1,10 @@
-// カスタムコードの表示を許可する
+// 初期表示のコントロールスクリプトーーーーーーーーーーーーーーーーーー
+// カスタム要素の出力 ＋ 余白エリアの除去
 var A = document.getElementsByClassName("content-area")[0];
 var B = document.getElementsByClassName("super-navbar")[0];
 B.after(A);
 A.style["margin-top"]=0;
 A.style["visibility"]="visible";
-
-
-// トップページの呼び出しーーーーーーーーーーーーーーーーーーーーーーーー
-
-const page_call_property={
-  url: 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec', 
-  callback: "top_page",
-  教室ID : newData["ページタイプ"] == "school"?newData["教室ID"]:"",
-  会員ID : newData["ページタイプ"] == "teacher"?newData["会員ID"]:"",
-}
-call_fetchData(page_call_property);
 
 var target = document.getElementById("page-content");
 target.innerHTML = `
@@ -23,39 +13,112 @@ target.innerHTML = `
 </div>
 `
 
-
 // サイドメニューの設定スクリプトーーーーーーーーーーーーーーーーーーーー
-let menuData
-if(newData["ページタイプ"] == "school"){
-menuData = [
-  // ページ設定
-  { id: "top-menu", title: "トップページ", hasChildElements: false, callback: "top_page", url: 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec' },
-  { id: "shift-menu", title: "シフト依頼ページ", hasChildElements: true, callback: "shift_page", url: 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec' },
-  { id: "kintai-menu", title: "勤怠確認ページ", hasChildElements: true, callback: "kintai_page", url: 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec' },
-  { id: "profile-menu", title: "講師プロフィール", hasChildElements: true, callback: "profile_page", url: 'https://script.google.com/macros/s/AKfycbwGiAxM_6KK8T7qfRzZOLAIApa-1uLq9xm5iBe4ZyRDirHwTPmgoe4EkMYbNIAziFg/exec' },
+let menuData = [
+  { id: "top_page", title: "トップページ", class: "single-menu" },
+  { id: "shift_page", title: newData["ページタイプ"] == "school" ? "シフト依頼ページ" : "シフト管理ページ", class: "group-menu" },
+  { id: "kintai_page", title: "勤怠確認ページ", class: "group-menu" },
+  { id: "profile_page", title: "講師プロフィール", class: newData["ページタイプ"] == "school" ? "group-menu" : "single-menu" },
 ];
-}else if(newData["ページタイプ"] == "teacher"){
-  menuData = [
-    // ページ設定
-    { id: "top-menu", title: "トップページ", hasChildElements: false, callback: "top_page", url: 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec' },
-    { id: "shift-menu", title: "シフト管理ページ", hasChildElements: true, callback: "shift_page", url: 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec' },
-    { id: "kintai-menu", title: "勤怠確認ページ", hasChildElements: true, callback: "kintai_page", url: 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec' },
-    { id: "profile-menu", title: "講師プロフィール", hasChildElements: false, callback: "profile_page", url: 'https://script.google.com/macros/s/AKfycbwGiAxM_6KK8T7qfRzZOLAIApa-1uLq9xm5iBe4ZyRDirHwTPmgoe4EkMYbNIAziFg/exec' },
-  ];
-}
+
+
+// ここからサイドメニューの作成スクリプト
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+const childElementsData = newData["ページタイプ"] == "school" ? newData["講師名一覧"] : newData["教室名一覧"];
+const childElementsId = newData["ページタイプ"] == "school" ? newData["講師ID一覧"] : newData["教室ID一覧"];
+
+
+// ここからサイドメニューの作成スクリプト
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+const menu = document.querySelector(".menu");
+
+menuData.forEach((menuItemData) => {
+  const menuItem = document.createElement("li");
+  menuItem.classList.add(menuItemData.class);
+  menuItem.id = menuItemData.id;
+
+  menuItem.innerHTML = `
+    <div class="menu-item">${menuItemData.title}</div>
+    ${menuItemData.class == "group-menu"? `
+    <ul class="child-menu">
+      ${childElementsData.map((element, index) =>
+        `<li data-id="${childElementsId[index]}">${element}</li>`
+      ).join("")}
+    </ul>
+    `: ``}
+  `;
+
+  menu.appendChild(menuItem);
+});
+
+// ここからサイドメニューのイベントリスナースクリプト
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+const Menus = document.querySelectorAll(".menu-item")
+Menus.forEach((Menu) => {
+  Menu.addEventListener("click", (e) => {
+    document.querySelectorAll(".child-menu").forEach(element => {
+      element.style.display="none";
+    });
+  });
+});
+
+const groupMenus = document.querySelectorAll(".menu .group-menu .menu-item")
+groupMenus.forEach((groupMenu) => {
+  groupMenu.addEventListener("click", (e) => {
+    groupMenu.querySelector(".child-menu").style.display="block";
+  });
+});
+
+const singleMenus = document.querySelectorAll(".menu .single-menu");
+singleMenus.forEach((singleMenu) => {
+  singleMenu.addEventListener("click", () => {
+    const page_call_property={
+      "callback" : singleMenu.id,
+    }
+    call_fetchData(page_call_property);
+  });
+});
+
+const childMenus = document.querySelectorAll(".menu .group-menu .child-menu li");
+childMenus.forEach((childMenu) => {
+  childMenu.addEventListener("click", () => {
+    // 親のgroup-menuのIDを取得する
+    const parentId = childMenu.closest('.group-menu').id;
+
+    const page_call_property={
+      "callback" : parentId,
+      "講師名" : newData["ページタイプ"] == "school" ? childMenu.innerHTML : "",
+      "会員ID" : newData["ページタイプ"] == "school" ? childMenu.dataset.id : "",
+      "教室名" : newData["ページタイプ"] == "teacher" ? childMenu.innerHTML : "",
+      "教室ID" : newData["ページタイプ"] == "teacher" ? childMenu.dataset.id : "",
+    }
+    call_fetchData(page_call_property);
+  });
+});
+
+
 
 // GASを起動するためのスクリプトーーーーーーーーーーーーーーーーーーーー
 function call_fetchData(page_call_property) {
   const data = {
-    "会員ID": page_call_property["会員ID"],
-    "教室ID": page_call_property["教室ID"],
-    "講師名": page_call_property["講師名"],
-    "教室名": page_call_property["教室名"],
+    "講師名": newData["ページタイプ"] == "teacher" ? newData["姓"]+newData["名"] : page_call_property["講師名"],
+    "会員ID": newData["ページタイプ"] == "teacher" ? newData["会員ID"] : page_call_property["会員ID"],
+    "教室名": newData["ページタイプ"] == "school" ? cnewData["教室名"] : page_call_property["教室名"],
+    "教室ID": newData["ページタイプ"] == "school" ? cnewData["教室ID"] : page_call_property["教室ID"],
     "callback": page_call_property["callback"],
     "ページタイプ": newData["ページタイプ"]
   };
 
-  fetch(page_call_property["url"], {
+  let url;
+  switch (page_call_property["callback"]) {
+    case "profile_page":
+      url = 'https://script.google.com/macros/s/AKfycbwGiAxM_6KK8T7qfRzZOLAIApa-1uLq9xm5iBe4ZyRDirHwTPmgoe4EkMYbNIAziFg/exec'; break;
+    default:
+      url = 'https://script.google.com/macros/s/AKfycbwmCc5XeYXWjCXLiztYc45LFdaX-bdzjbET8KXZcWbfF5TVwKk-dQeokyOfKivAwlB9/exec';
+  }
+  
+
+  fetch(url, {
     method: 'POST',
     headers: { 
       'Content-Type': 'text/plain',
@@ -74,92 +137,6 @@ function call_fetchData(page_call_property) {
     }
   });
 }
-
-
-// ここからサイドメニューの作成スクリプト
-// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-const menu = document.querySelector(".menu");
-menuData.forEach((menuItemData) => {
-  // メニューの大枠を作成ーーーーーーーーーーーーーーーーーーーーーーー
-  const menuItem = document.createElement("li");
-  menuItem.classList.add("group-menu");
-  menuItem.id = menuItemData.id;
-
-  const menuTitle = document.createElement("div");
-  menuTitle.classList.add("menu-item");
-  menuTitle.textContent = menuItemData.title;
-  menuItem.appendChild(menuTitle);
-
-  // 小要素があるメニューーーーーーーーーーーーーーーーーーーーーーーー
-  if (menuItemData.hasChildElements) {
-    const childMenu = document.createElement("ul");
-    childMenu.classList.add("child-menu");
-    menuItem.appendChild(childMenu);
-
-    menuTitle.addEventListener("click", () => {
-      pulldown(childMenu);
-    });
-
-    // 子要素のプロパティ配列を準備するーーーーーーーーーーーーーーーーーー
-    const childElementsData = [
-      ...(newData["ページタイプ"] === "school"
-        ? newData["講師名一覧"].map((element, index) => ({
-            label2:"講師名",
-            elements: element,
-            label: "会員ID",
-            ids: newData["講師ID一覧"][index],
-          }))
-        : []),
-      ...(newData["ページタイプ"] === "teacher"
-        ? newData["教室名一覧"].map((element, index) => ({
-            label2:"教室名",
-            elements: element,
-            label: "教室ID",
-            ids: newData["教室ID一覧"][index],
-          }))
-        : []),
-    ];
-    console.log(childElementsData)
-
-    // 小要素の作成、アドイベントリスナーの設定を行うーーーーーーーーーーー
-    childElementsData.forEach((childElementData) => {
-      const childElement = document.createElement("li");
-      childElement.textContent = childElementData["elements"];
-      childElement.addEventListener("click", (e) => {
-        page_call_property["url"]= menuItemData.url;
-        page_call_property["callback"] = menuItemData.callback;
-        page_call_property[childElementData["label"]] =childElementData["ids"];
-        page_call_property[childElementData["label2"]]=childElementData["elements"];
-        call_fetchData(page_call_property);
-      });
-      childMenu.appendChild(childElement);
-    });
-  }else{
-  // 親要素のみのメニューーーーーーーーーーーーーーーーーーーーーーーー
-    menuTitle.addEventListener("click", (e) => {
-      page_call_property["url"]= menuItemData.url;
-      page_call_property["callback"] = menuItemData.callback;
-      call_fetchData(page_call_property);
-    });
-    menuTitle.addEventListener("click", () => {
-      pulldown();
-    });
-  }
-  menu.appendChild(menuItem);
-});
-
-// プルダウンの設定ーーーーーーーーーーーーーーーーーーーーーーーーーーー
-function pulldown(childMenu){
-  var target = document.querySelectorAll(".child-menu");
-  target.forEach(element => {
-    element.style.display="none";
-  });
-  if(childMenu !=null){
-    childMenu.style.display="block"
-  }
-}
-
-
 
 
 
@@ -185,10 +162,9 @@ function initializeLayout() {
   }
 }
 
+
 // 初回読み込み時にinitializeLayout関数を呼び出す
 initializeLayout()
-
-
 
 // リサイズイベント時にも同じような処理を行う
 let lastWindowWidth = window.innerWidth;
