13fc7cd916740dc4f04870e05231bb352121b9d1
ryohey <ryohey0405@gmail.com>
2023-06-03 06:52:23 +0900

    a

--- spot-js/31_page_chat.js
+++ spot-js/31_page_chat.js
@@ -18,38 +18,74 @@ for( let hiddenElement of hiddenElements){
 readCheck(form)
 
 
-
-
-
-
-
-
 async function handleSubmit(event) {
-  // フォーム送信時アクションの設定する関数ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-  event.preventDefault(); // デフォルトの送信をキャンセル
-  const form = event.target;
-  const formData = new FormData(form);
-  const data = {};
-  // FormDataオブジェクトから連想配列に変換
-  for (const [key, value] of formData.entries()) {
-      data[key] = value;
-  }
+    // フォーム送信時アクションの設定する関数ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    event.preventDefault(); // デフォルトの送信をキャンセル
+    const form = event.target;
+    const formData = new FormData(form);
+    const data = {};
+    // FormDataオブジェクトから連想配列に変換
+    for (const [key, value] of formData.entries()) {
+        data[key] = value;
+    }
 
-  // データの整理ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー 
-  data["ページタイプ"] = newData["ページタイプ"]
-  data["タイムスタンプ"] = new Date().toLocaleString("ja-JP", {
-    year: "numeric", month: "2-digit", day: "2-digit",
-    hour: "2-digit", minute: "2-digit", second: "2-digit",
-  });
+    // データの整理ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー 
+    data["ページタイプ"] = newData["ページタイプ"]
+    data["タイムスタンプ"] = new Date().toLocaleString("ja-JP", {
+        year: "numeric", month: "2-digit", day: "2-digit",
+        hour: "2-digit", minute: "2-digit", second: "2-digit",
+    });
 
-  const response = await fetch( "https://script.google.com/macros/s/AKfycbwc5wu1HOL0RkT7WOWO5jrLbVBskvNEiqV8gwias6gMCs0yFCSW45t6-lp8VbelwRl3/exec", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-    body: JSON.stringify(data),
-    mode: 'cors', //CORS対応
-  });
+    // メッセージの出力ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+    // メッセージの日付を取得
+    let date = new Date(data["タイムスタンプ"]);
+    let formattedDate = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+
+    // メッセージの最後の日付を取得
+    const dayElements = document.querySelectorAll(".day-line .date-text");
+    const lastDate = dayElements[dayElements.length -1].textContent
+    let addHTML
+
+
+    // 日付が初めてのものであれば、<div class="day-line">を追加
+    if (formattedDate !== lastDate) {
+        addHTML +=`<div class="day-line"><hr>
+                    <span class="date-text">${formattedDate}</span>
+                <hr></div>`;
+        previousDate = formattedDate;
+    }
+
+    // メッセージの送信者によってclassを設定、　既読フラグでフラグ設定、　提出時刻（時分）
+
+    let timestamp = date.getHours() + ":" + ("0" + date.getMinutes()).slice(-2);
+    let icon = data["ページタイプ"]== "school" ? "fa-solid fa-school" : "fas fa-user";
+
+
+    // メッセージHTMLを追加
+    addHTML +=`<div class="chat-log sent">
+                <div class="icon-box"><div class="icon-container"><i class="${icon}"></i></div></div>
+                <div class="chat-box">${data["メッセージ"]}</div>
+                <div class="info-box">
+                <span class="read-flag">  </span>
+                <span class="timestamp">${timestamp}
+                </span></div>
+            </div>`;
+
+    const chatArea = document.querySelector(".chat-log-area")
+    chatArea.insertAdjacentHTML("beforeend",addHTML)
+
+
+    // データの送信ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー 
+
+    const response = await fetch( "https://script.google.com/macros/s/AKfycbwc5wu1HOL0RkT7WOWO5jrLbVBskvNEiqV8gwias6gMCs0yFCSW45t6-lp8VbelwRl3/exec", {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'text/plain',
+        },
+        body: JSON.stringify(data),
+        mode: 'cors', //CORS対応
+    });
 
 };
 
@@ -66,6 +102,8 @@ async function readCheck(form) {
     // データの整理ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー 
     data["ページタイプ"] = newData["ページタイプ"]
     data["既読フラグ"] = true
+
+    // データの送信ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー 
   
     const response = await fetch( "https://script.google.com/macros/s/AKfycbwc5wu1HOL0RkT7WOWO5jrLbVBskvNEiqV8gwias6gMCs0yFCSW45t6-lp8VbelwRl3/exec", {
       method: 'POST',
