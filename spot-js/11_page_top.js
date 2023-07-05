c990a242b1c9255cf516caee35352306a6373e6a
ryohey <ryohey0405@gmail.com>
2023-07-05 17:33:41 +0900

    a

--- spot-js/11_page_top.js
+++ spot-js/11_page_top.js
@@ -162,7 +162,7 @@ rows.forEach(row => {
     if(formId == "approvalForm" && newstart!=null){
 
 
-      form.querySelector("#勤務時間の変更-wrapper").innerHTML='<span style="color:#800000;">講師から勤務時間の変更申請があります。<br>修正の必要があれば、修正の上、「勤務を承認する」ボタンをクリックしてください。</span>'
+      form.querySelector("#勤務時間の変更-wrapper").innerHTML='<span style="color:#800000;display:inline-block">講師から勤務時間の変更申請があります。<br>修正の必要があれば、修正の上、「勤務を承認する」ボタンをクリックしてください。</span>'
       form.querySelector("#勤務開始時間-wrapper").style.display="inline-block"
       form.querySelector("#勤務終了時間-wrapper").style.display="inline-block"
       form.querySelector("#休憩時間-wrapper").style.display="inline-block"
