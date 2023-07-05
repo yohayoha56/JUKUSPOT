521aa9cb22effe0e7ee64122ea594db99b0a42d5
ryohey <ryohey0405@gmail.com>
2023-07-05 19:35:59 +0900

    a

--- spot-js/11_page_top.js
+++ spot-js/11_page_top.js
@@ -244,13 +244,11 @@ async function handleSubmit(event,remarks,row) {
   let isValid = true;
   switch (formId) {
     case 'approvalForm': 
-      if (!data["勤務時間の変更"]){ isValid = false;
-        showValidationError(form.querySelector("#勤務時間の変更-wrapper"), "変更有無の回答をしてください");
-      } if(data["勤務時間の変更"] === "変更あり" &&( !data["勤務開始時間_hour"] || !data["勤務開始時間_hour"])){isValid = false;
+      if( !data["勤務開始時間_hour"] || !data["勤務開始時間_hour"]){isValid = false;
         showValidationError(form.querySelector("#勤務開始時間-wrapper"), "有効な時間にしてください");
-      } if(data["勤務時間の変更"] === "変更あり" &&( !data["勤務終了時間_hour"] || !data["勤務終了時間_hour"])){isValid = false;
+      } if(!data["勤務終了時間_hour"] || !data["勤務終了時間_hour"]){isValid = false;
         showValidationError(form.querySelector("#勤務終了時間-wrapper"), "有効な時間にしてください");
-      } if(data["勤務時間の変更"] === "変更あり" && !data["休憩時間"]) {isValid = false;
+      } if(!data["休憩時間"]) {isValid = false;
         showValidationError(form.querySelector("#休憩時間-wrapper"), "有効な時間にしてください");
       }
       break;
