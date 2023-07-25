8c310d35ae8bf61e0d2bbe745e48c62361010f93
ryohey <ryohey0405@gmail.com>
2023-07-25 17:09:44 +0900

    a

--- spot-js/41_page_prof.js
+++ spot-js/41_page_prof.js
@@ -15,6 +15,7 @@ function profile_page(page_call_property) {
         <button id="send-message">この内容で講師にメッセージを送る</button>
     </div>
     `
+    
     const target = document.querySelector(".profile-tables")
     target.insertAdjacentText("beforebegin",firstContactHTML)
 
