e717dff64a29e64a94927dfe0a85f0051ab01c80
yohayoha56 <131740199+yohayoha56@users.noreply.github.com>
2024-06-04 17:29:06 +0900

    Update 03_common_guide.js

--- production/spot-js/03_common_guide.js
+++ production/spot-js/03_common_guide.js
@@ -3,8 +3,9 @@ function insertGuide(page_call_property){
     let headerTitle = newData["ページタイプ"] == "school" ? 
     `｜${newData["教室名"]}管理ページ`: `｜${newData["姓"]}${newData["名"]}先生マイページ`;
 
-    // ページによって、トップ部分にガイドの挿入を行うための関数
-    const {title, guide} = getPageTitleAndGuide(newData["ページタイプ"], page_call_property["callback"]);
+    // const {title, guide} = getPageTitleAndGuide(newData["ページタイプ"], page_call_property["callback"]);
+    const title = "";
+    const guide = "";
 
 
     // ページタイトル＋サービスロゴ＋ガイドの挿入ーーーーーーーーーーーーーーーーーーーーーーーーーーー
