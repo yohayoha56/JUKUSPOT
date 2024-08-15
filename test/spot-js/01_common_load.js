794efc78dcf45ebaacdeec4718dc868c3213b863
YOICHIRO HARAGUCHI <131739992+gucchon001@users.noreply.github.com>
2024-08-15 12:33:41 +0900

    Update 01_common_load.js

--- test/spot-js/01_common_load.js
+++ test/spot-js/01_common_load.js
@@ -42,7 +42,7 @@ function handleTimeout() {
 }
 
 let paramsID, paramsP;
-console.log("01_common_load staging_202408141714");
+console.log("01_common_load staging_202408151233");
 
 // 3. waitForProperties関数の呼び出しと処理
 waitForProperties().then((propertiesContainer) => {
