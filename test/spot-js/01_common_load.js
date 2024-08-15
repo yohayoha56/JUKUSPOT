a9a06d51f6428c8d4127ce4de13f9427aa447148
yohayoha56 <131740199+yohayoha56@users.noreply.github.com>
2024-08-15 12:41:05 +0900

    Update 01_common_load.js

--- test/spot-js/01_common_load.js
+++ test/spot-js/01_common_load.js
@@ -42,7 +42,7 @@ function handleTimeout() {
 }
 
 let paramsID, paramsP;
-console.log("01_common_load staging_202408151233");
+console.log("01_common_load staging_202408151240");
 
 // 3. waitForProperties関数の呼び出しと処理
 waitForProperties().then((propertiesContainer) => {
